9ff46927880d49e20b0edcb3c4b6f353ef784df0
Sathish <skumar.sathih@gmail.com>
2025-07-26 15:15:07 +0530

    My First commit

---
+++ Link.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+   struct node *next;
+   int data;
+}node;
+
+node * getNewNode(int data)
+{
+     node *newNode = NULL;
+     newNode = (node *) malloc(sizeof(node));
+     if(newNode)
+     {
+        newNode->data = data;
+        newNode->next = NULL;
+     }
+     return newNode;
+}
+int insert(node *root, int data)
+{
+     node *ptr = root;
+     node *temp;
+     if(root  == NULL)
+     {
+         temp = getNewNode(data);
+	 if(temp != NULL)
+         {
+             ptr->next = temp;
+         }
+         else
+            return -1;
+     } 
+}
+int main()
+{
+    node *head;
+}
